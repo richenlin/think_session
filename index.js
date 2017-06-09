8a51a610e3f2a3964d73a629726363c6e5bb0494
chenjp <chenjp@yunheit.com>
2017-06-09 19:07:24 +0800

    重新封装Session

--- index.js
+++ index.js
@@ -10,10 +10,32 @@ const Session = require('./lib/session.js');
 
 module.exports = function (options) {
     return function (ctx, next) {
-        ctx.session = function(name, value, timeout) {
-            const instance = new Session(options);
-            instance.run(name, value, timeout);
-        };
+        think.app.once('appReady', () => {
+            ctx.session = function(name, value, timeout) {
+                //判断think_cache中间件
+                if (!think.cache.adapter) {
+                    ctx.throw(500, 'please install think_cache middleware');
+                }
+
+                //生成session实例
+                let instance;
+                if (!ctx._sessionKey) {
+                    instance = new Session(options, ctx.cookie);
+                    ctx._sessionKey = instance;
+                } else {
+                    instance = ctx._sessionKey;
+                }
+
+                //调用session方法
+                if (value === undefined) {
+                    return instance.get(name);
+                } else if (value === null) {
+                    return instance.rm(name);
+                } else {
+                    return instance.set(name, value, timeout);
+                }
+            };
+        });
         return next();
     };
 };
--- src/session.js
+++ src/session.js
@@ -9,7 +9,7 @@ const lib = require('think_lib');
 const crypto = require('crypto');
 
 var cacheOptions = {
-    cache_type: 'File', //数据缓存类型 File,Redis,Memcache
+    cache_type: 'file', //数据缓存类型 file,redis,memcache
     cache_key_prefix: 'ThinkKoa:', //缓存key前置
     cache_timeout: 6 * 3600, //数据缓存有效期，单位: 秒
     cache_file_suffix: '.json', //File缓存方式下文件后缀名
@@ -18,91 +18,120 @@ var cacheOptions = {
 };
 
 module.extends = class {
-    constructor(options = {}) {
+    constructor(options = {}, cookie) {
         this.options = lib.extend({
             session_name: 'thinkkoa', //session对应的cookie名称
-            session_type: 'Redis', //session存储类型 File,Redis,Memcache
+            session_type: 'redis', //session存储类型 file,redis,memcache
             session_path: '', //File类型下文件存储位置，默认为Temp目录
             session_options: {}, //session对应的cookie选项
             session_sign: '', //session对应的cookie使用签名
             session_timeout: 24 * 3600, //服务器上session失效时间，单位：秒
         }, options);
+        this.cookie = cookie;
     }
 
-    //执行session动作
-    run(name, value, timeout) {
-        const instance = this.getUniqueInstance();
+    /**
+     * Session写入
+     * @param name
+     * @param value
+     * @param timeout
+     * @returns {Promise}
+     */
+    set(name, value, timeout) {
+        let instance = this.getInstance();
 
-        if (value === undefined) {
-            return instance.get(name).then(val => {
-                return lib.isJSONStr(val) ? JSON.parse(val) : val;
-            });
-        } else if (value === null) {
-            return instance.rm(name);
-        } else {
-            return instance.set(name, (lib.isBoolean(value) || lib.isNumber(value) || lib.isString(value)) ? value : JSON.stringify(value), timeout || this.options.cache_timeout);
+        let oldData = this.get(name);
+        let content = {
+            [name]: value,
+            timeout: timeout
+        };
+
+        if (!lib.isEmpty(oldData)) {
+            content = lib.extend(false, oldData, content);
         }
+
+        //to do ...
+
+        return instance.set(name, (lib.isBoolean(value) || lib.isNumber(value) || lib.isString(value)) ? value : JSON.stringify(value), timeout || this.options.cache_timeout);
     }
 
-    //获取当前ctx的session实例
-    getUniqueInstance() {
-        //直接返回
-        if (ctx.sessionKey) {
-            return ctx.sessionKey;
-        }
+    /**
+     * Session读取
+     * @param name
+     */
+    get(name) {
+        let instance = this.getInstance();
+        return instance.get(name).then(val => {
+            return lib.isJSONStr(val) ? JSON.parse(val) : val;
+        });
+    }
 
-        //判断think_cache中间件
-        if (!think.cache.adapter) {
-            ctx.throw(500, 'please install think_cache middleware');
-        }
+    /**
+     * Session移除
+     * @param name
+     */
+    rm(name) {
+        let instance = this.getInstance();
+        return instance.rm(name);
+    }
 
+    /**
+     * 获取session实例
+     * @returns {*}
+     */
+    getInstance() {
         //validate cookie sign
         let sessionName = this.options.session_name;
         let sessionSign = this.options.session_sign;
 
-        let cookie = ctx.cookie(sessionName);
+        let cookie = this.cookie(sessionName);
         if (cookie && sessionSign) {
             cookie = this.cookieUnsign(cookie, sessionSign);
-            //set cookie to ctx._cookie
-            if (cookie) {
-                ctx._cookie[sessionName] = cookie;
-            }
         }
 
-        let sessionCookie = cookie;
         //generate session cookie when cookie is not set
         if (!cookie) {
             cookie = this.cookieUid(32);
-            sessionCookie = cookie;
             //sign cookie
             if (sessionSign) {
                 cookie = this.cookieSign(cookie, sessionSign);
             }
-            //将生成的sessionCookie放在ctx._cookie对象上，方便程序内读取
-            ctx._cookie[sessionName] = sessionCookie;
-            ctx.cookie(sessionName, cookie, { length: 32, httponly: true });
+            this.cookie(sessionName, cookie, { length: 32, httponly: true });
         }
 
         const handle = this.options.handle || think.cache.adapter;
-        cacheOptions.cache_type = lib.ucFirst(this.options.session_type);
-        ctx.sessionKey = new handle(cacheOptions);
-        return ctx.sessionKey;
+        cacheOptions.cache_type = this.options.session_type;
+        return new handle(cacheOptions);
     }
 
-    //生成cookie签名
+    /**
+     * 生成cookie签名
+     * @param val
+     * @param secret
+     * @returns {string}
+     */
     cookieSign(val, secret = '') {
         secret = crypto.createHmac('sha256', secret).update(val).digest('base64');
         secret = secret.replace(/\=+$/, '');
         return val + '.' + secret;
     }
 
-    //解析cookie签名
+    /**
+     * 解析cookie签名
+     * @param val
+     * @param secret
+     * @returns {*}
+     */
     cookieUnsign(val, secret) {
         let str = val.slice(0, val.lastIndexOf('.'));
         return cookieSign(str, secret) === val ? str : '';
     }
 
-    //生成uid
+    /**
+     * 生成uid
+     * @param length
+     * @returns {string}
+     */
     cookieUid(length) {
         let str = crypto.randomBytes(Math.ceil(length * 0.75)).toString('base64').slice(0, length);
         return str.replace(/[\+\/]/g, '_');
